04bace21ec7efde613e87f88c488897612656a8f
keerthana31999 <keerthanacherry123@gmail.com>
2021-04-06 12:46:35 +0530

    Added::valid last name of user

--- userValidationRegex.java
+++ userValidationRegex.java
@@ -4,28 +4,48 @@ import java.util.regex.*;
 public class UserRegistration {
 
 	static String fName;
+	static String lName;
+
+   static String firstName(){
+      Scanner str1 = new Scanner(System.in);
+      System.out.println("Enter your first name in ('K'eerthana) format");
+      fName = str1.nextLine();
+      return fName;
+   }
 
 
-	static String firstName(){
-		Scanner str1 = new Scanner(System.in);
-		System.out.println("Enter your first name in ('K'eerthana) format: ");
-		fName = str1.nextLine();
-		return fName;
-	}
-
-	public static void main(String args[]) {
-
-		System.out.println("Welcome to User Registration Problem!");
-
-		Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",UserRegistration.firstName());
+	static String lastName(){
+      Scanner str2 = new Scanner(System.in);
+      System.out.println("Enter your last name in ('J'ujjavarapu) format");
+      lName = str2.nextLine();
+      return lName;
+   }
 
 
-		while(firstName == false) {
-			System.out.println("Error! Invalid input from user please enter your First-Name in valid format(ex-'S'anketh).");
-			firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",UserRegistration.firstName());
-		}
+   public static void main(String args[]) {
+
+      System.out.println("Welcome to User Registration Problem!");
 
 
-		System.out.println("First Name: " + fName);
+      Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",UserRegistration.firstName());
+
+
+      Boolean lastName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",UserRegistration.lastName());
+
+
+      while(firstName == false) {
+         System.out.println("Error! Invalid input from user please enter your First-Name in valid format(ex-'S'anketh).");
+         firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",UserRegistration.firstName());
+      }
+
+		while(lastName == false) {
+         System.out.println("Error! Invalid input from user please enter your Last-Name in valid format(ex-'C'higurupalli).");
+         lastName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",UserRegistration.lastName());
+      }
+
+
+      System.out.println("First Name: " + fName);
+		System.out.println("Last Name: " + lName);
+
 	}
 }
